bba0fda74bee871fe4699c42cfad353a351a5990
shauryam <shaurya_mittal@ymail.com>
2016-11-28 21:01:18 -0800

    Factory Method Implemeted for Difficulty level (points Incrementor)

--- js/singlePlayer_script.js
+++ js/singlePlayer_script.js
@@ -1,13 +1,22 @@
-var duration = getParameterByName('difficulty');
-console.log(duration);
 $(function(){
+
+  var points = 00;
+  var difficulty = getParameterByName('difficulty');
+  var pointsIncrementor = setPointsIncrementor(difficulty);
+  console.log(pointsIncrementor);
 	var draggedWord;
+
+  //This will pick the strings randomly from the array 'compressionStr' present in compressionStrings.js file
    var str = compressionStr[Math.floor(Math.random()*compressionStr.length)];
+   var strWithoutSymbols = removeSymbols(str);
+   //Code to split the string to individual words and put them on screen for dragging
    var arr = str.split(" ");
    $.each(arr, function( index, value ) {
-  		console.log(value);
   		$( ".string" ).append( "<p class='draggable'>"+value+"</p>" );
-	});
+	 });
+
+   //This array has been created to compare draggedWords with words without symbols like period, comma or exclamation.
+   var arrWithoutSymbols = strWithoutSymbols.split(" ");
 
    $(".draggable").draggable();
 
@@ -19,13 +28,17 @@ $(function(){
       start: function( event, ui ) {
         console.log(ui.helper[0].innerText);
         draggedWord = ui.helper[0].innerText;
+        //to remove symbols like period, comma or exclamation from dragged word
+        draggedWord = removeSymbols(draggedWord);
       }
   });
+  //Dragging code ends above
 
+  
   $( "#droppable" ).droppable({
       drop: function( event, ui ) {   
-        count = 0;
-        $.each(arr, function( index, value ) {
+        count = 0;        
+        $.each(arrWithoutSymbols, function( index, value ) {
         if(value == draggedWord){
           count++;
         }
@@ -36,13 +49,14 @@ $(function(){
     else{
       $(this).animateCss('bounce');
       var text = $("#draggable p").innerText;
-      console.log(text);
     }
       }
     });
 
 });
 
+
+//this code animates the box
 $.fn.extend({
     animateCss: function (animationName) {
         var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
@@ -51,7 +65,9 @@ $.fn.extend({
         });
     }
 });
+//box animation code ends here
 
+//code for timer starts here which uses the function 'getParameterByName()' to fetch duration from query string
 var seconds = getParameterByName('duration')*1000;
 var time = new Date().getTime() + seconds;
 $('#clock').countdown(time, {elapse: true})
@@ -62,20 +78,10 @@ $('#clock').countdown(time, {elapse: true})
     $this.html(event.strftime(' %S '));
   }
 });
+//timer code ends here
 
 
-//funtion to parse the query string
-function getParameterByName(name, url) {
-    if (!url) {
-      url = window.location.href;
-    }
-    name = name.replace(/[\[\]]/g, "\\$&");
-    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-        results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return '';
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-}
+
 
 
 
