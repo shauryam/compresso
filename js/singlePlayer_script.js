5d1d56ab14d453a76a3ee24cff1729c5ac0a9c8b
shauryam <shaurya_mittal@ymail.com>
2016-11-29 04:56:58 -0800

    Points and other features added. Only result page with Hashmap left

--- js/singlePlayer_script.js
+++ js/singlePlayer_script.js
@@ -1,60 +1,74 @@
 $(function(){
 
   var points = 00;
-  var difficulty = getParameterByName('difficulty');
-  var pointsIncrementor = setPointsIncrementor(difficulty);
-  console.log(pointsIncrementor);
+  var difficulty = getParameterByName('difficulty'); 
+  var pointsIncrementor = setPointsIncrementor(difficulty); //Implemented by factory design pattern
 	var draggedWord;
 
   //This will pick the strings randomly from the array 'compressionStr' present in compressionStrings.js file
    var str = compressionStr[Math.floor(Math.random()*compressionStr.length)];
-   var strWithoutSymbols = removeSymbols(str);
+   var strWithoutSymbols = removeSymbols(str); //will remove symbols 
+
    //Code to split the string to individual words and put them on screen for dragging
    var arr = str.split(" ");
+   counter=0;
    $.each(arr, function( index, value ) {
-  		$( ".string" ).append( "<p class='draggable'>"+value+"</p>" );
+  		$( ".string" ).append( "<p id='draggable"+counter+"'>"+value+"</p>" );      
+      counter++;
 	 });
 
    //This array has been created to compare draggedWords with words without symbols like period, comma or exclamation.
    var arrWithoutSymbols = strWithoutSymbols.split(" ");
 
-   $(".draggable").draggable();
-
-   $(".draggable").draggable({ 
-      revert: true,
-      containment: "#play-area",
-      opacity: 0.50,
-      cursor: "crosshair",
-      start: function( event, ui ) {
-        console.log(ui.helper[0].innerText);
-        draggedWord = ui.helper[0].innerText;
-        //to remove symbols like period, comma or exclamation from dragged word
-        draggedWord = removeSymbols(draggedWord);
-      }
-  });
+   //following loop adds the feature of dragging all the words and also gives each word a unique ID
+   for(var i=0; i<counter; i++){
+      var id = "#draggable" + i;
+      $(id).draggable();
+      $(id).draggable({ 
+          revert: true,
+          containment: "#play-area",
+          cursor: "crosshair",
+          start: function( event, ui ) {
+            console.log(ui.helper[0].innerText);
+            draggedWord = ui.helper[0].innerText;
+            //to remove symbols like period, comma or exclamation from dragged word
+            draggedWord = removeSymbols(draggedWord);
+          }
+      });
+    }
+   
   //Dragging code ends above
 
   
+  //droppable is the compression box and the following code has the logic to check if dragged word is compressable
   $( "#droppable" ).droppable({
       drop: function( event, ui ) {   
-        count = 0;        
+        count = 0; //count for matched words
+        var matchingIndexes = []; // this array is used to track the indexes of the words that will match the dragged word
         $.each(arrWithoutSymbols, function( index, value ) {
-        if(value == draggedWord){
-          count++;
+          if(value == draggedWord){
+            count++;
+            matchingIndexes.push(index); 
+          }
+        });  
+        if(count<2){ // 2 or more including the word dragged
+          $(this).effect("shake");
         }
-    });  
-    if(count<2){
-      $(this).effect("shake");
-    }
-    else{
-      $(this).animateCss('bounce');
-      var text = $("#draggable p").innerText;
-    }
+        else{
+          $(this).animateCss('bounce');
+          var text = $("#draggable p").innerText;
+          var score = $("#score").html();
+          var word = ui.helper[0];
+          $.each(matchingIndexes, function( index, value ) { //this loop greys out all the matched words
+            var elementSelector = "#draggable" + value;
+            $(elementSelector).draggable("disable");
+            $(elementSelector).css("opacity", 0.3);
+          });
+          $("#score").html(parseInt(score) + pointsIncrementor);
+        }
       }
     });
-
 });
-
 
 //this code animates the box
 $.fn.extend({
