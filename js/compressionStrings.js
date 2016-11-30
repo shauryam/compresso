dba4322d1e9c632040974a3d3d83a3bc8e61fe8b
YashasviKomma <yashasvi.komma@sjsu.edu>
2016-11-29 17:13:36 -0800

    Update compressionStrings.js

--- js/compressionStrings.js
+++ js/compressionStrings.js
@@ -1,5 +1,24 @@
 //Array of compression strings
 var compressionStr = [
   "This is the text that has to be compressed. This is going to be compressed by the player",
-  "Java is an object oriented language. The languages which are object oriented like java has OOPS concepts like abstraction, inheritace etc. The inheritace feature gives the advantage of code reuse."
+  "Java is an object oriented language. The languages which are object oriented like java has OOPS concepts like abstraction, inheritace etc. The inheritace feature gives the advantage of code reuse",
+  "Change the world to technology, technology effects every field , it is not just about computers anymore, and ot is not just about tablets or smart phones, technology is about medicine, energy, transportation",
+  "Computing and technology has transformed society from the laptop we use to work and play, to medicine, to home as IoT, to GPS to self driving cars. Computing effects every one and every field, we need every one fluent in computing and technolgy ",
+  "I am thinking about something much more important than bombs, I am thinking about computer",
+  "To access an object we use reference variable, reference variable can be of only one type, reference variable cannot be changed, reference variable can be reassigned to other objects, reference variable can be declared as a class or interface type",
+  "Agile software development refers to a group of software development methods which are based on iterative development. Agile methods or agile processes generally promote a disciplined project management process",
+  "betty botteutr bought a bit of butter but she said butter is bitter if I put in my batter it will make my batter bitter but a bit of better butter will make my batter better so she bought a bit of butter better than her bitter butter made her bitter batter better so it was better betty botter bought a bit of better butter",
+  "peter piper picked a peck of pickled peppers, a peck of pickled peppers peter pipper picked, if peter piper picked a peck of pickled peppers, where are the pickled peppers peter piper picked",
+  "I wish to wish the wish you wish to wish, but if u wish to wish the witch wishes I wont wish the wish you wish to wish"
+  "this is this  cat this is is cat this is how cat this is to cat this is keep cat this is an cat this is idiot cat this is busy cat this is for cat this is forty cat this is seconds cat",
+  "esau wood saw wood. esau wood would saw wood. oh, the wood that wood would saw ! one day esau wood saw a saw saw wood as no other wood saw wood ever saw wouls saw wood, wood ever saw saw wood, wood never saw a wood saw that would saw wood like the wood saw wood saw would saw wood now esau wood saws with that saw he saw saw wood ",
+  "a problem of solving a problem, but when a problem solves a problem without any problem then the problem is not at all a problem"
+  "luke luck likes lakes luckes duck likes lakes luke luck licks lakes lukes duck licks lakes duck takes licks in lakes luke luck takes licks in lakes duck likes"
+  "How many cookies could a good cook cook if a good cook could cook cookies ? a good cook could cook as much cookies as a good cook who could cook cookies"
+  "As I was in Arkansas I saw a saw that could out saw any saw I ever saw saw . If you happen to be Arkansas and see a saw that can out saw the saw I saw saw Id like to see the saw you saw saw",
+  "the big black bug bit the big black bear, but the big black bear bit the big black bug",
+  "a fly and flea flew into flue, said the fly to the flea  what shall we do ? , let us fly said the flea said the fly shall we flee so they flew through a flaw in the flue"
+  
+  
+  
 ];
