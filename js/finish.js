53efe656be661b1ab73f7bb5f938bad1bd8dcfbc
shauryam <shaurya_mittal@ymail.com>
2016-11-30 17:35:37 -0800

    Test push

--- js/finish.js
+++ js/finish.js
@@ -1,6 +1,5 @@
 function finish() {
 	for(var i = 0;  i<numberOfWords; i++){
-    console.log("Heyy");
     var elementSelector = "#draggable" + i;
     $(elementSelector).draggable("disable");
   }
